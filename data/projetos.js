452541b8f67cec34008d509d9d6ed804c22c4bde
Mateus Oliveira <mateussfer09@gmail.com>
2025-04-08 18:56:14 -0300

    Update projetos.js

--- data/projetos.js
+++ data/projetos.js
@@ -4,8 +4,8 @@ const projetos = [
         titulo: "Controlly",
         descricao: "Um sistema de controle de finanças que registrar receitas e despesas e salva no LocalStorage.",
         tecnologias: ["HTML", "CSS", "JAVASCRIPT"], // Array de strings
-        link_repositorio: "https://",
-        link_demo: "https://www.youtube.com",
+        link_repositorio: "#",
+        link_demo: "#",
         banner: "../data/banners/controlly.png"
     }
 ];
